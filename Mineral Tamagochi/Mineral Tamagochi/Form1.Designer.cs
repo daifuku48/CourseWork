d0ffff9f9f54401436945a0a725082dcf38e7c27
danil <natalya.haritonov.72@gmail.com>
2022-09-15 18:36:03 +0300

    Init

--- Mineral Tamagochi/Mineral Tamagochi/Form1.Designer.cs
+++ Mineral Tamagochi/Mineral Tamagochi/Form1.Designer.cs
@@ -29,32 +29,34 @@ namespace Mineral_Tamagochi
         /// </summary>
         private void InitializeComponent()
         {
-            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // domainUpDown1
+            // label1
             // 
-            this.domainUpDown1.Location = new System.Drawing.Point(123, 162);
-            this.domainUpDown1.Name = "domainUpDown1";
-            this.domainUpDown1.Size = new System.Drawing.Size(120, 23);
-            this.domainUpDown1.TabIndex = 0;
-            this.domainUpDown1.Text = "domainUpDown1";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(658, 405);
-            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
