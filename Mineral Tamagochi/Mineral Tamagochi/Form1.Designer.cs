772e198af3677b4be5393d539c529a612b733c95
danil <natalya.haritonov.72@gmail.com>
2022-09-15 18:33:47 +0300

    Init

--- Mineral Tamagochi/Mineral Tamagochi/Form1.Designer.cs
+++ Mineral Tamagochi/Mineral Tamagochi/Form1.Designer.cs
@@ -29,13 +29,32 @@ namespace Mineral_Tamagochi
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.SuspendLayout();
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Location = new System.Drawing.Point(123, 162);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.domainUpDown1.TabIndex = 0;
+            this.domainUpDown1.Text = "domainUpDown1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(658, 405);
+            this.Controls.Add(this.domainUpDown1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
     }
 }
 
