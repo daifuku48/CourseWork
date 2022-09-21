6992cec1b4889c64b01b0562e6a78f7855256f6e
Bazl1 <dazman3243243@gmail.com>
2022-09-21 21:01:11 +0300

    Добавление Menu

--- Tamagochi WPF/Tamagochi WPF/MainWindow.xaml.cs
+++ Tamagochi WPF/Tamagochi WPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Tamagochi_WPF
 {
@@ -20,9 +21,54 @@ namespace Tamagochi_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer;
+
+        double MenuWidth;
+        bool hiddenMenu;
+
         public MainWindow()
         {
             InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0 , 1);
+            timer.Tick += Timer_Tick;
+
+            MenuWidth = sideMenu.Width;
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (hiddenMenu)
+            {
+                sideMenu.Width += MenuWidth/10;
+                if (sideMenu.Width >= MenuWidth)
+                {
+                    timer.Stop();
+                    hiddenMenu = false;
+                }
+            }
+            else
+            {
+                sideMenu.Width -= MenuWidth/10;
+                if (sideMenu.Width <= 0)
+                {
+                    timer.Stop();
+                    hiddenMenu = true;
+                }
+            }
+        }
+
+        private void Button_Menu(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+  
+
+        private void MenuPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
         }
     }
 }
--- Tamagochi WPF/Tamagochi WPF/obj/Debug/Tamagochi WPF_Content.g.i.cs
+++ Tamagochi WPF/Tamagochi WPF/obj/Debug/Tamagochi WPF_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icons/menu.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icons/exit.png")]
 
 
