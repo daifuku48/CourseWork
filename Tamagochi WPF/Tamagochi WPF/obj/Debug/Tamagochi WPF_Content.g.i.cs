2ab60c52ee8302e07dec84ec6a1410470169f5b9
danil <natalya.haritonov.72@gmail.com>
2022-10-03 01:40:06 +0300

    добавил крестик

--- Tamagochi WPF/Tamagochi WPF/obj/Debug/Tamagochi WPF_Content.g.i.cs
+++ Tamagochi WPF/Tamagochi WPF/obj/Debug/Tamagochi WPF_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icons/exit1.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("exit3.png")]
 
 
